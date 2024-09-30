08b17425482c24f88fac6c1a98e4d9260f8fd7d8
RDx <rodrigo.apaza.m@tecsup.edu.pe>
2024-09-30 17:39:02 -0500

    feat: add PostApiService interface with endpoints

---
+++ app/src/main/java/com/josuerdx/network/PostApiService.kt
@@ -0,0 +1,14 @@
+package com.josuerdx.network
+
+import com.josuerdx.models.PostModel
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface PostApiService {
+    @GET("posts")
+    suspend fun getUserPosts(): List<PostModel>
+
+    @GET("posts/{id}")
+    suspend fun getUserPostById(@Path("id") id: Int): PostModel
+
+}
